8d41c15739596c7c53ea9b5b5d6970e9507dcc75
Starcky Molina <starckymolina@gmail.com>
2016-08-26 08:15:43 -0300

    clase 1 - calculadora

---
+++ Nivel 2 - Front end TP/clase 1 - calculadora/calculadora.js
@@ -0,0 +1,70 @@
+var operacion = prompt("Que desea hacer:", "(+, -, *, /)");
+
+var numero1 = prompt("ingrese su primer numero");
+var numero2 = prompt("ingrese su segundo numero");
+
+var resultado = eval(numero1+operacion+numero2);
+alert(+resultado.toFixed(2));
+
+
+// var numero1 = eval(prompt("ingrese su primer numero"));
+// var numero2 = eval(prompt("ingrese su segundo numero"));
+// var suma = numero1 + numero2;
+// var resta = numero1 - numero2;
+// var multiplicacion = numero1 * numero2;
+// var division = numero1 / numero2;
+
+// if (operacion == "sumar"){
+// 	alert("su resultado es" + " " + suma);
+// };
+
+// if (operacion == "restar"){
+// 	alert("su resultado es" + " " + resta);
+// };
+
+// if (operacion == "multiplicar"){
+// 	alert("su resultado es" + "\n" + multiplicacion);
+// };
+
+// if (operacion == "dividir"){
+// 	alert("su resultado es" + " " + division);
+// };
+
+
+
+
+// if (operacion == "sumar"){
+
+// 	var numero1 = eval(prompt("ingrese su primer numero"));
+// 	var numero2 = eval(prompt("ingrese su segundo numero"));
+// 	var resultado = numero1 + numero2;
+// 	alert("su resultado es" + " " + resultado);
+// };
+
+// if (operacion == "restar"){
+
+// 	var numero1 = eval(prompt("ingrese su primer numero"));
+// 	var numero2 = eval(prompt("ingrese su segundo numero"));
+// 	var resultado = numero1 - numero2;
+// 	alert("su resultado es" + " " + resultado);
+// };
+
+// if (operacion == "multipicar"){
+
+// 	var numero1 = eval(prompt("ingrese su primer numero"));
+// 	var numero2 = eval(prompt("ingrese su segundo numero"));
+// 	var resultado = numero1 * numero2;
+// 	alert("su resultado es" + " " + resultado);
+// };
+
+// if (operacion == "dividir"){
+
+// 	var numero1 = eval(prompt("ingrese su primer numero"));
+// 	var numero2 = eval(prompt("ingrese su segundo numero"));
+// 	var resultado = numero1 / numero2;
+// 	alert("su resultado es" + " " + resultado);
+// };
+
+
+
+
